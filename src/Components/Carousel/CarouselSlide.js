1645a37f38b00c63f7826310f7f3ca5d11121e74
Thomas Otim <totim444@hotmail.co.uk>
2022-04-29 16:37:41 +0300

    Carousel section of the home page styled

---
+++ src/Components/Carousel/CarouselSlide.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Carousel from "react-elastic-carousel";
+import styled from "styled-components";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 576, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 992, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+];
+
+const CarouselSlide = ({ children }) => {
+  return (
+    <Wrapper>
+      {/* <hr className="seperator" /> */}
+      <div className="carousel-wrapper">
+        <Carousel
+          breakPoints={breakPoints}
+          enableAutoPlay
+          autoPlaySpeed={2000}
+          enableSwipe
+          enableMouseSwipe
+        >
+          {children}
+        </Carousel>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  font-family: sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* height: 100vh; */
+  /* padding: 0 8rem; */
+
+  .carousel__item {
+    /* display: flex; */
+    /* justify-content: center; */
+    /* align-items: center; */
+    height: 25rem;
+    /* width: 100%; */
+    color: #fff;
+    margin: 15px;
+    font-size: 4rem;
+  }
+
+  .carousel__item img {
+    box-shadow: 1px 10px 16px black;
+    border: 3px solid var(--tertiaryColor);
+    /* border-radius: 10px; */
+    width: 30rem;
+    display: flex;
+  }
+
+  /* .seperator {
+    width: 100%;
+  } */
+
+  .controls-wrapper {
+    width: 100%;
+  }
+
+  .rec-carousel-item:focus {
+    outline: none;
+    box-shadow: inset 0 0 1px 0px var(--primaryColor);
+  }
+
+  .rec-arrow {
+    background: transparent !important;
+    color: var(--primaryColor);
+    font-size: 3.5rem;
+  }
+
+  .rec-dot_active {
+    background-color: var(--primaryColor) !important;
+  }
+
+  .rec-dot:hover,
+  .rec-dot_active,
+  .rec-dot:focus {
+    box-shadow: 0 0 1px 3px var(--tertiaryColor) !important;
+  }
+
+  @media (max-width: 576px) {
+    .rec-arrow {
+      display: none;
+    }
+  }
+`;
+
+export default CarouselSlide;
---
+++ src/Components/ImageCarousel.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "styled-components";
+import CarouselSlide from "./Carousel/CarouselSlide";
+import Title from "./Title";
+import cleo from "../assets/cleo.png";
+
+const ImageCarousel = () => {
+  return (
+    <Wrapper>
+      <Title title="Gallery" />
+      <CarouselSlide>
+        <div className="carousel__item">
+          <img src={cleo} alt="cleo" className="carousel__img" />
+        </div>
+        <div className="carousel__item">
+          <img src={cleo} alt="cleo" className="carousel__img" />
+        </div>
+        <div className="carousel__item">
+          <img src={cleo} alt="cleo" className="carousel__img" />
+        </div>
+        <div className="carousel__item">
+          <img src={cleo} alt="cleo" className="carousel__img" />
+        </div>
+      </CarouselSlide>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  padding: 2rem 8rem;
+
+  h3 {
+    font-size: 3rem;
+    color: var(--white);
+  }
+
+  .carousel__img {
+    /* display: block; */
+    width: 100%;
+    object-fit: cover;
+  }
+
+  @media (max-width: 576px) {
+    padding: 3rem 1rem;
+  }
+`;
+
+export default ImageCarousel;
